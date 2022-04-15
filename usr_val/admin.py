cf786ce1d58b6f4ab559c4cbc24977f042c6cffb
bishwajit <arin17bishwa@gmail.com>
2022-04-15 11:16:17 +0530

    modified admin panel for "User" table

--- usr_val/admin.py
+++ usr_val/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import Student, Teacher, ResearchStatement
 
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_active')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(ResearchStatement)
